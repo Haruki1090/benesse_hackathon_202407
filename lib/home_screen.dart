dea7a522395d3226beec99722d085090ccbdaa51
Haruki1090 <haruharu.com.109607@gmail.com>
2024-07-13 20:37:55 +0900

    const解除

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,22 +1,25 @@
 import 'package:benesse_hackathon_202407/conponents/custom_bottom_navigation_bar.dart';
+import 'package:benesse_hackathon_202407/screens/class_screen.dart';
+import 'package:benesse_hackathon_202407/screens/profile_screen.dart';
+import 'package:benesse_hackathon_202407/screens/school_screen.dart';
+import 'package:benesse_hackathon_202407/screens/team_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
 
 import 'login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  User? user;
+  auth.User? user;
   Map<String, dynamic>? userData;
+  int _selectedIndex = 0; // 現在の選択中のタブインデックス
 
   @override
   void initState() {
@@ -43,8 +46,21 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> screens = [
+      SchoolScreen(userData: userData), // 学校画面
+      ClassScreen(userData: userData), // クラス画面
+      TeamScreen(userData: userData), // チーム画面
+      ProfileScreen(userData: userData), // プロフィール画面
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
@@ -58,18 +74,12 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Center(
         child: userData == null
             ? const CircularProgressIndicator()
-            : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Welcome, ${userData!['username']}'),
-                  Text('School: ${userData!['school']}'),
-                  Text('Club: ${userData!['club']}'),
-                  Text('Grade: ${userData!['grade']}'),
-                  Text('Education Level: ${userData!['educationLevel']}'),
-                ],
-              ),
+            : screens[_selectedIndex],
       ),
-      bottomNavigationBar: const CustomBottomNavigationBar(),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
+      ),
     );
   }
 }

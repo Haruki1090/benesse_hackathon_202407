a8afd11ddaee2c5a0e6a57f7888ef22bfe279562
Haruki1090 <haruharu.com.109607@gmail.com>
2024-07-13 20:57:35 +0900

    AppBarの設置

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,25 +1,47 @@
+import 'package:benesse_hackathon_202407/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
+  final auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
   final Map<String, dynamic>? userData;
 
-  const ProfileScreen({super.key, this.userData});
+  ProfileScreen({super.key, this.userData});
+
+  Future<void> _logout(BuildContext context) async {
+    await _auth.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginScreen()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: userData == null
-          ? const CircularProgressIndicator()
-          : Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Welcome, ${userData!['username']}'),
-                Text('School: ${userData!['school']}'),
-                Text('Club: ${userData!['club']}'),
-                Text('Grade: ${userData!['grade']}'),
-                Text('Education Level: ${userData!['educationLevel']}'),
-              ],
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('プロフィール'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => _logout(context),
+          ),
+        ],
+      ),
+      body: Center(
+        child: userData == null
+            ? const CircularProgressIndicator()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Welcome, ${userData!['username']}'),
+                  Text('School: ${userData!['school']}'),
+                  Text('Club: ${userData!['club']}'),
+                  Text('Grade: ${userData!['grade']}'),
+                  Text('Education Level: ${userData!['educationLevel']}'),
+                ],
+              ),
+      ),
     );
   }
 }

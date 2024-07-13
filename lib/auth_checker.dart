031ad3607f04dfb7f0abec1668f821cb3cdcad92
Haruki1090 <haruharu.com.109607@gmail.com>
2024-07-13 15:27:40 +0900

    ✨認証管理機能を作成

---
+++ lib/auth_checker.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+import 'login_screen.dart';
+
+class AuthChecker extends StatelessWidget {
+  const AuthChecker({super.key});
+
+  //  ログイン状態を監視し、ログインしている場合はHomeScreenを表示し、ログインしていない場合はLoginScreenを表示する。
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasData) {
+          return HomeScreen();
+        } else {
+          return LoginScreen();
+        }
+      },
+    );
+  }
+}

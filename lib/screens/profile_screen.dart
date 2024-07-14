b764f41f1ae11fd9c245a7a65782bce374ce0830
Haruki1090 <haruharu.com.109607@gmail.com>
2024-07-14 14:49:50 +0900

    ✨プロフィール画面を作成

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,46 +1,76 @@
-import 'package:benesse_hackathon_202407/login_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
-  final auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
-  final Map<String, dynamic>? userData;
-
-  ProfileScreen({super.key, this.userData});
-
-  Future<void> _logout(BuildContext context) async {
-    await _auth.signOut();
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => LoginScreen()),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('プロフィール'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () => _logout(context),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 40), // 上部に余白を追加
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage(
+                  'assets/profile_placeholder.png'), // プロフィール画像のパスを指定
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'UserName', // ユーザー名を表示
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'user@example.com', // ユーザーのメールアドレスを表示
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            const SizedBox(height: 20),
+            _buildProfileItem(Icons.school, '学校名', '東高校'),
+            _buildProfileItem(Icons.group, '部活', '野球部'),
+            _buildProfileItem(Icons.grade, '学年', '2年生'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // ログアウト処理など
+              },
+              child: const Text('ログアウト'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                textStyle:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProfileItem(IconData icon, String title, String subtitle) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: [
+          Icon(icon, size: 30),
+          const SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                subtitle,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
           ),
         ],
-      ),
-      body: Center(
-        child: userData == null
-            ? const CircularProgressIndicator()
-            : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Welcome, ${userData!['username']}'),
-                  Text('School: ${userData!['school']}'),
-                  Text('Club: ${userData!['club']}'),
-                  Text('Grade: ${userData!['grade']}'),
-                  Text('Education Level: ${userData!['educationLevel']}'),
-                ],
-              ),
       ),
     );
   }

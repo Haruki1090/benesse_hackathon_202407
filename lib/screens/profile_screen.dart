24bc846bb0f4e9c25f13e3fe701a8c54be75b689
Haruki1090 <haruharu.com.109607@gmail.com>
2024-07-14 15:40:52 +0900

    ✨学校トップページ作成

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,47 +1,130 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ProfileScreen extends StatelessWidget {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final ImagePicker _picker = ImagePicker();
+  User? _user;
+  Map<String, dynamic>? _userData;
+  File? _imageFile;
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserData();
+  }
+
+  Future<void> _getUserData() async {
+    _user = _auth.currentUser;
+    if (_user != null) {
+      DocumentSnapshot userDoc =
+          await _firestore.collection('users').doc(_user!.uid).get();
+      setState(() {
+        _userData = userDoc.data() as Map<String, dynamic>?;
+      });
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+      await _uploadImage();
+    }
+  }
+
+  Future<void> _uploadImage() async {
+    if (_imageFile == null || _user == null) return;
+    String filePath = 'user_profile_images/${_user!.uid}.png';
+    try {
+      await FirebaseStorage.instance.ref(filePath).putFile(_imageFile!);
+      String downloadURL =
+          await FirebaseStorage.instance.ref(filePath).getDownloadURL();
+      await _firestore
+          .collection('users')
+          .doc(_user!.uid)
+          .update({'profileImageUrl': downloadURL});
+      setState(() {
+        _userData!['profileImageUrl'] = downloadURL;
+      });
+    } catch (e) {
+      print('Error uploading profile image: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_userData == null) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.fromLTRB(16, 64, 16, 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const SizedBox(height: 40), // 上部に余白を追加
-            const CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage(
-                  'assets/profile_placeholder.png'), // プロフィール画像のパスを指定
+            const SizedBox(height: 40),
+            GestureDetector(
+              onTap: _pickImage,
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: _userData!['profileImageUrl'] != null
+                    ? NetworkImage(_userData!['profileImageUrl'])
+                    : null,
+                child: _userData!['profileImageUrl'] == null
+                    ? SvgPicture.asset('assets/default_profile.svg',
+                        fit: BoxFit.cover)
+                    : null,
+              ),
             ),
             const SizedBox(height: 20),
-            const Text(
-              'UserName', // ユーザー名を表示
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            Text(
+              _userData!['username'] ?? 'UserName',
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 10),
-            const Text(
-              'user@example.com', // ユーザーのメールアドレスを表示
-              style: TextStyle(fontSize: 16, color: Colors.grey),
+            Text(
+              _user?.email ?? 'user@example.com',
+              style: const TextStyle(fontSize: 16, color: Colors.grey),
             ),
             const SizedBox(height: 20),
-            _buildProfileItem(Icons.school, '学校名', '東高校'),
-            _buildProfileItem(Icons.group, '部活', '野球部'),
-            _buildProfileItem(Icons.grade, '学年', '2年生'),
+            _buildProfileItem(
+                Icons.school, '学校名', _userData!['school'] ?? '不明'),
+            _buildProfileItem(Icons.group, '部活', _userData!['club'] ?? '不明'),
+            _buildProfileItem(Icons.grade, '学年', _userData!['grade'] ?? '不明'),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // ログアウト処理など
+                _auth.signOut();
+                Navigator.of(context).pushReplacementNamed('/login');
               },
-              child: const Text('ログアウト'),
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
+                backgroundColor: Colors.white,
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
                 textStyle:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
               ),
+              child: const Text('ログアウト'),
             ),
           ],
         ),
--- lib/screens/school_screen.dart
+++ lib/screens/school_screen.dart
@@ -1,25 +1,117 @@
 import 'package:flutter/material.dart';
 
 class SchoolScreen extends StatelessWidget {
-  final Map<String, dynamic>? userData;
-
-  const SchoolScreen({super.key, this.userData});
+  const SchoolScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: userData == null
-          ? const CircularProgressIndicator()
-          : Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Welcome, ${userData!['username']}'),
-                Text('School: ${userData!['school']}'),
-                Text('Club: ${userData!['club']}'),
-                Text('Grade: ${userData!['grade']}'),
-                Text('Education Level: ${userData!['educationLevel']}'),
-              ],
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 64, 16, 16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'ベネッセ高校',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                '7月',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              _buildEventItem('9', '期末テスト', Colors.white, Colors.black),
+              _buildEventItem('10', '期末テスト', Colors.white, Colors.black),
+              _buildEventItem('11', '期末テスト', Colors.white, Colors.black),
+              _buildEventItem(
+                  '28', '陸上競技部 地区記録会', Colors.green, Colors.white, true),
+              const SizedBox(height: 40),
+              const Text(
+                'イベント',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              _buildEventDetailItem('部活動見学会があります！'),
+              _buildEventDetailItem('吹奏楽部 定期演奏会開催について'),
+              _buildEventDetailItem('【注意】夏季休業期間の自習室開講時間'),
+              _buildEventDetailItem('大学オープンキャンパス日程まとめ'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEventItem(
+      String day, String event, Color bgColor, Color textColor,
+      [bool isBold = false]) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 20,
+            backgroundColor: bgColor,
+            child: Text(
+              day,
+              style: TextStyle(
+                color: textColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
             ),
+          ),
+          const SizedBox(width: 10),
+          Text(
+            event,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildEventDetailItem(String detail) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Text(
+          detail,
+          style: const TextStyle(
+            fontSize: 16,
+          ),
+          overflow: TextOverflow.ellipsis,
+          maxLines: 1,
+        ),
+      ),
     );
   }
 }
